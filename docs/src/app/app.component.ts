1e95d9948f0024ed44143d3d41b881a5681116d8
Christophe <chgaborieau@sqli.com>
2020-07-11 23:40:44 +0200

    Maj documentation

--- docs/src/app/app.component.ts
+++ docs/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'appComponents';
+  components: any;
+
+  constructor(
+    private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('/assets/applicationComponents.json')
+    .subscribe(data => this.components = data);
+  }
+
+
+
 }
